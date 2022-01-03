57cd197de3354524ca440acd6644a08d9c41de3f
Pink-piggy <80244476+Pink-piggy@users.noreply.github.com>
2022-01-03 23:02:27 +0800

    Add files via upload

--- Circle.java
+++ Circle.java
@@ -1,27 +1,34 @@
-package haha;
+package Unit11;
 
-public class Circle {
-	private double radius = 1;
-	private static int numberOfObjects = 0;
-	
+public class Circle extends GeometricObject{
+	private double radius;
 	public Circle() {
-		numberOfObjects++;
 	}
-	
-	public Circle(double newRadius) {
-		radius = newRadius;
-		numberOfObjects++;
+	public Circle(double radius) {
+		this.radius=radius;
+	}
+	public Circle(double radius,String color,boolean filled) {
+		this.radius=radius;
+		setColor(color);
+		setFilled(filled);
 	}
 	public double getRadius() {
 		return radius;
 	}
-	public void setRadius(double newRadius) {
-		radius = (newRadius >= 0) ? newRadius : 0;
-	}
-	public static int getNumberOfObjects() {
-		return numberOfObjects;
+	public void setRadius(double radius) {
+		this.radius=radius;
 	}
 	public double getArea() {
-		return radius * radius * Math.PI;
+		return radius*radius*Math.PI;
+	}
+	public double getDiameter() {
+		return 2*radius;
+	}
+	public double getPerimeter() {
+		return 2*radius*Math.PI;
+	}
+	public void printCircle() {
+		System.out.println("The circle is created "+getDateCreated()+" and the radius is "+radius);
 	}
 }
+
